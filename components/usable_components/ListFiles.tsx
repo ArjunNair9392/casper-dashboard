f5b68882e04b2c85c8fb4d36e4baa24991ed6cb4
devmeht92 <devmehta0492@gmail.com>
2024-05-14 10:17:51 -0700

    fix search, sort and pagination

--- components/usable_components/ListFiles.tsx
+++ components/usable_components/ListFiles.tsx
@@ -1,7 +1,6 @@
-import { useEffect, useState, useRef, Fragment } from 'react';
+import { useEffect, useState, Fragment } from 'react';
 import 'tippy.js/dist/tippy.css';
-import { setPageTitle } from '../../store/themeConfigSlice';
-import { useDispatch, useSelector } from 'react-redux';
+import { useDebouncedValue } from '@mantine/hooks';
 import IconUserPlus from '@/components/Icon/IconUserPlus';
 import { Dialog, Transition } from '@headlessui/react';
 import { googleAuthURL } from '../../helpers/constants';
@@ -23,20 +22,22 @@ interface FileData {
 interface Props {
     onSave: ([]) => void;
 }
+const PAGE_SIZES = [10, 20, 30, 50, 100];
 
 const ListFiles: React.FC<Props> = ({ onSave }) => {
-    const dispatch = useDispatch();
-    const { user, error, isLoading } = useUser();
-    const [rowData, setRowData] = useState<FileData[]>([]);
+    const { user } = useUser();
     const [modal2, setModal2] = useState(false);
     const [page, setPage] = useState(1);
-    const PAGE_SIZES = [10, 20, 30, 50, 100];
     const [pageSize, setPageSize] = useState(PAGE_SIZES[0]);
     const [initialRecords, setInitialRecords] = useState<FileData[]>([]);
-    const [recordsData, setRecordsData] = useState<FileData[]>([]);
+    const [recordsData, setRecordsData] = useState<FileData[]>(sortBy([...initialRecords.slice(0, pageSize)], 'name'));
     const [search, setSearch] = useState('');
     const [selectedRecords, setSelectedRecords] = useState<any>([]);
     const [refreshCode, setRefreshCode] = useState('');
+    const [sortStatus, setSortStatus] = useState<DataTableSortStatus>({
+        columnAccessor: 'name',
+        direction: 'asc',
+    });
 
     // const convertToFileRowData = (files: any[]) => {
     //     return files.map((file) => ({
@@ -55,7 +56,6 @@ const ListFiles: React.FC<Props> = ({ onSave }) => {
         const code = urlParams.get('code');
         if (code) {
             // Use the code for further processing
-            console.log("AuthCode", code);
             setModal2(true);
             setRefreshCode(code);
         }
@@ -69,7 +69,7 @@ const ListFiles: React.FC<Props> = ({ onSave }) => {
                 .then(response => {
                     // Handle successful response
                     console.log(response.data);
-                    setRecordsData(response.data);
+                    setInitialRecords(response.data);
                 })
                 .catch(error => {
                     // Handle error
@@ -84,11 +84,6 @@ const ListFiles: React.FC<Props> = ({ onSave }) => {
         onSave(selectedRecords);
     };
 
-    const [sortStatus, setSortStatus] = useState<DataTableSortStatus>({
-        columnAccessor: 'name',
-        direction: 'asc',
-    });
-
     useEffect(() => {
         setPage(1);
     }, [pageSize]);
@@ -100,16 +95,16 @@ const ListFiles: React.FC<Props> = ({ onSave }) => {
     }, [page, pageSize, initialRecords]);
 
     useEffect(() => {
-        console.log('Search Call', search);
-        setInitialRecords(() => {
-            return rowData.filter((item) => {
-                const itemName = item.name.toLowerCase().trim();
-                const searchTerm = search.toLowerCase().trim();
-                return (
-                    itemName.includes(searchTerm)
-                );
-            });
-        });
+        const from = (page - 1) * pageSize;
+        const to = from + pageSize;
+        const filteredRecords = initialRecords.filter((file) => {
+            if (search !== '' && !file.name.toLowerCase().includes(search.trim().toLowerCase())) {
+                return false;
+            }
+            return true;
+        })
+        console.log({ filteredRecords });
+        setRecordsData(filteredRecords.slice(from, to));
     }, [search]);
 
     useEffect(() => {
@@ -150,7 +145,7 @@ const ListFiles: React.FC<Props> = ({ onSave }) => {
                                 leaveFrom="opacity-100 scale-100"
                                 leaveTo="opacity-0 scale-95"
                             >
-                                <Dialog.Panel as="div" className="panel my-8 w-full max-w-xl overflow-hidden rounded-lg border-0 p-0 text-black dark:text-white-dark">
+                                <Dialog.Panel as="div" className="panel my-8 w-full overflow-hidden rounded-lg border-0 p-0 text-black dark:text-white-dark">
                                     <div className="flex items-center justify-between bg-[#fbfbfb] px-5 py-3 dark:bg-[#121c2c]">
                                         <h5 className="text-lg font-bold">Select PDF files for Casper to read</h5>
                                         <button type="button" className="text-white-dark hover:text-dark" onClick={() => setModal2(false)}>
@@ -160,14 +155,14 @@ const ListFiles: React.FC<Props> = ({ onSave }) => {
                                     <div className="p-5">
                                         <div className="panel mt-6">
                                             <div className="mb-5 flex flex-col gap-5 md:flex-row md:items-center">
-                                                <h5 className="text-lg font-semibold dark:text-white-light">Checkbox</h5>
+                                                <h5 className="text-lg font-semibold dark:text-white-light">Files</h5>
                                                 <div className="ltr:ml-auto rtl:mr-auto">
                                                     <input type="text" className="form-input w-auto" placeholder="Search..." value={search} onChange={(e) => setSearch(e.target.value)} />
                                                 </div>
                                             </div>
                                             <div className="datatables">
                                                 <DataTable
-                                                    className="table-hover whitespace-nowrap"
+                                                    className="table-hover"
                                                     records={recordsData}
                                                     columns={[
                                                         {
@@ -181,6 +176,7 @@ const ListFiles: React.FC<Props> = ({ onSave }) => {
                                                             ),
                                                         },
                                                     ]}
+                                                    loadingText={"Please wait while we fetch your files"}
                                                     highlightOnHover
                                                     totalRecords={initialRecords.length}
                                                     recordsPerPage={pageSize}
@@ -188,12 +184,11 @@ const ListFiles: React.FC<Props> = ({ onSave }) => {
                                                     onPageChange={(p) => setPage(p)}
                                                     recordsPerPageOptions={PAGE_SIZES}
                                                     onRecordsPerPageChange={setPageSize}
+                                                    paginationText={({ from, to, totalRecords }) => `Showing  ${from} to ${to} of ${totalRecords} entries`}
                                                     sortStatus={sortStatus}
                                                     onSortStatusChange={setSortStatus}
                                                     selectedRecords={selectedRecords}
                                                     onSelectedRecordsChange={setSelectedRecords}
-                                                    minHeight={200}
-                                                    paginationText={({ from, to, totalRecords }) => `Showing  ${from} to ${to} of ${totalRecords} entries`}
                                                 />
                                             </div>
                                         </div>
